71aa242e875e563819dda932e9e4f1f3fb6ef250
Vignesh Shetty <vigneshshetty.in@gmail.com>
2022-03-25 21:44:17 +0530

    PORT

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const router = require("./router/router");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
